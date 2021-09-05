89ea65983dc612efe17bffb331714aeb527adfde
jefferymoulton <jeff@jefferymoulton.com>
2021-09-04 23:23:09 -0700

    Refactored code to separate modules. Created update functionality.

--- models/profile.go
+++ models/profile.go
@@ -1,9 +1,66 @@
 package models
 
 type Profile struct {
-	ID       uint   `json:"id" gorm:"primary_key"`
-	Email    string `json:"email" gorm:"unique"`
-	Nickname string `json:"nickname"`
-	Name     string `json:"name"`
-	Picture  string `json:"picture"`
+	ID          uint   `json:"id" gorm:"primary_key"`
+	Email       string `json:"email" gorm:"unique"`
+	Nickname    string `json:"nickname"`
+	Name        string `json:"name"`
+	Picture     string `json:"picture"`
+	Description string `json:"description"`
+}
+
+type ProfileInput struct {
+	Email       string `json:"email" binding:"required"`
+	Nickname    string `json:"nickname" binding:"required"`
+	Name        string `json:"name" binding:"required"`
+	Picture     string `json:"picture"`
+	Description string `json:"description"`
+}
+
+func CreateProfile(input ProfileInput) (Profile, error) {
+	profile := mapInput(input)
+
+	if err := DB.Create(&profile).Error; err != nil {
+		return Profile{}, err
+	}
+
+	return profile, nil
+}
+
+func UpdateProfile(input ProfileInput) (Profile, error) {
+	existing, err := GetProfileByEmail(input.Email)
+
+	if err != nil {
+		return Profile{}, err
+	}
+
+	profile := mapInput(input)
+	profile.ID = existing.ID
+
+	if err := DB.Save(&profile).Error; err != nil {
+		return Profile{}, err
+	}
+
+	return profile, nil
+}
+
+func GetProfileByEmail(email string) (Profile, error) {
+	var profile Profile
+
+	if err := DB.Where("email = ?", email).First(&profile).Error; err != nil {
+		return Profile{}, err
+	}
+
+	return profile, nil
+}
+
+func mapInput(input ProfileInput) Profile {
+	var profile Profile
+	profile.Email = input.Email
+	profile.Name = input.Name
+	profile.Nickname = input.Nickname
+	profile.Picture = input.Picture
+	profile.Description = input.Description
+
+	return profile
 }
--- routes/profiles.go
+++ routes/profiles.go
@@ -3,74 +3,72 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"writerxl-api/dto"
 	"writerxl-api/models"
 )
 
-type CreateProfileInput struct {
-	Email    string `json:"email" binding:"required"`
-	Nickname string `json:"nickname" binding:"required"`
-	Name     string `json:"name" binding:"required"`
-	Picture  string `json:"picture"`
+type ProfileDTO struct {
+	Email       string `json:"email"`
+	Nickname    string `json:"nickname"`
+	Name        string `json:"name"`
+	Picture     string `json:"picture"`
+	Description string `json:"description"`
 }
 
-type ProfileDTO struct {
-	Email    string `json:"email"`
-	Nickname string `json:"nickname"`
-	Name     string `json:"name"`
-	Picture  string `json:"picture"`
-}
+var path = "/api/profile"
 
 func LoadProfileRoutes(e *gin.Engine) {
-	e.POST("/api/profile", CreateProfile)
-	e.GET("/api/profile", FindProfiles)
+	e.POST(path, CreateProfile)
+	e.PUT(path, UpdateProfile)
 
-	e.GET("/api/profile/:email", GetProfile)
+	e.GET(path+"/:email", GetProfile)
 }
 
 func CreateProfile(c *gin.Context) {
-	var input CreateProfileInput
+	var input models.ProfileInput
 
 	if err := c.ShouldBindJSON(&input); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	profile := models.Profile{
-		Email:    input.Email,
-		Nickname: input.Nickname,
-		Name:     input.Name,
-		Picture:  input.Picture,
-	}
-	models.DB.Create(&profile)
+	profile, err := models.CreateProfile(input)
 
-	// TODO: Return an error if one occurs (duplicate entry, etc.)
-	c.JSON(http.StatusOK, mapDTO(profile))
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, dto.MapDTO(profile))
 }
 
-func FindProfiles(c *gin.Context) {
-	var profiles []models.Profile
-	models.DB.Find(&profiles)
+func UpdateProfile(c *gin.Context) {
+	var input models.ProfileInput
 
-	c.JSON(http.StatusOK, gin.H{"profiles": profiles})
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	profile, err := models.UpdateProfile(input)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, dto.MapDTO(profile))
 }
 
 func GetProfile(c *gin.Context) {
 	var profile models.Profile
 
-	if err := models.DB.Where("email = ?", c.Param("email")).First(&profile).Error; err != nil {
+	profile, err := models.GetProfileByEmail(c.Param("email"))
+
+	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Profile was not found"})
 		return
 	}
 
-	c.JSON(http.StatusOK, mapDTO(profile))
-}
-
-func mapDTO(user models.Profile) ProfileDTO {
-	var dto ProfileDTO
-	dto.Email = user.Email
-	dto.Name = user.Name
-	dto.Nickname = user.Nickname
-	dto.Picture = user.Picture
-
-	return dto
+	c.JSON(http.StatusOK, dto.MapDTO(profile))
 }
